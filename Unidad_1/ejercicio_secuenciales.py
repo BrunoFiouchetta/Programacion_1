cda9468024afa5745fdd9ab0ad6862d8e0ababd9
BrunoFiouchetta <brunofiouchetta12@gmail.com>
2025-08-21 00:11:04 -0300

    tp empezados

---
+++ Unidad_1/ejercicio_secuenciales.py
@@ -0,0 +1,21 @@
+"""
+
+Calculadora de propinas en un restaurante 
+Un restaurante quiere ayudar a sus clientes a calcular cuánto dejar de propina según el 
+monto de la cuenta. 
+El programa debe: 
+✓ Pedir al usuario el monto total de la cuenta. 
+✓ Calcular la propina sugerida al 10%. 
+✓ Calcular la propina sugerida al 15%. 
+✓ Calcular el total a pagar en ambos casos (cuenta + propina). 
+✓ Mostrar todos los resultados en pantalla. 
+Ejemplo de ejecución 
+Ingrese el monto de la cuenta: 3500 
+Propina sugerida (10%): 350.0 
+Total a pagar (10%): 3850.0 
+Propina sugerida (15%): 525.0 
+Total a pagar (15%): 4025.0
+
+"""
+
+
