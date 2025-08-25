8c28924ec2aed0a8c89ee40b236e3bf8a55adc5c
BrunoFiouchetta <brunofiouchetta12@gmail.com>
2025-08-25 15:37:23 -0300

    ..

--- 01 ESTRUCTURAS SECUENCIALES/fiouchetta_tp_1_aulavirtual.py
+++ 01 ESTRUCTURAS SECUENCIALES/fiouchetta_tp_1_aulavirtual.py
@@ -49,9 +49,18 @@ print(f"{segundos} segundos equivalen a {horas} horas")
 print("TABLA_MULTIPLICAR:")
 print("ingrese un numero")
 numero = int(input("numero: "))
-for i in range(1, 11):
-    resultado = numero * i
-    print(f"{numero} x {i} = {resultado}")
+
+print(f"tabla de multiplicar del {numero}:")
+print(f"{numero} x 1 = {numero * 1}")
+print(f"{numero} x 2 = {numero * 2}")
+print(f"{numero} x 3 = {numero * 3}")
+print(f"{numero} x 4 = {numero * 4}")
+print(f"{numero} x 5 = {numero * 5}")
+print(f"{numero} x 6 = {numero * 6}")
+print(f"{numero} x 7 = {numero * 7}")
+print(f"{numero} x 8 = {numero * 8}")
+print(f"{numero} x 9 = {numero * 9}")
+print(f"{numero} x 10 = {numero * 10}")
 
 
 #7) Crear un programa que pida al usuario dos números enteros distintos del 0 y muestre por pantalla el resultado de sumarlos, dividirlos, multiplicarlos y restarlos.
