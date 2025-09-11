8bc3bf0950d5031a1ce8838c6972ef7247815bbf
BrunoFiouchetta <brunofiouchetta12@gmail.com>
2025-09-10 22:17:44 -0300

    cambios finales puno 9 y 10

--- 03 ESTRUCTURAS CONDICIONALES/fiouchetta_tp_03.py
+++ 03 ESTRUCTURAS CONDICIONALES/fiouchetta_tp_03.py
@@ -1,46 +1,45 @@
 
 #1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años, 
 #deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
-'''
+
 edad = int(input("ingrese su edad: "))
 
 if edad > 18:
     print("Es mayor de edad")
-'''
+
 #2) Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá
 #mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el mensaje “Desaprobado”.
-'''
+
 nota = float(input("Ingrese su nota: "))
 
 if nota >= 6:
     print("Aprobado")
 else:
     print("Desaprobado")
-'''
-'''
+
+"""
 3) Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
 número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
 contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
 operador de módulo (%) en Python para evaluar si un número es par o impar.
-'''
-'''
+"""
+
 num1 = int(input("Ingrese un numero par: "))
 
 if num1 % 2 == 0:
     print("ha ingresado un numero par")
 else:
     print("por favor ingrese un numero par")
-'''
 
-'''
+"""
 4) Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las
 siguientes categorías pertenece:
 ● Niño/a: menor de 12 años.
 ● Adolescente: mayor o igual que 12 años y menor que 18 años.
 ● Adulto/a joven: mayor o igual que 18 años y menor que 30 años.
 ● Adulto/a: mayor o igual que 30 años.
-'''
-'''
+"""
+
 edad = int(input("ingrese su edad: "))
 
 if edad < 12:
@@ -51,7 +50,7 @@ elif edad >= 18 and edad < 30:
     print("usted es un adulto.")
 else:
     print("usted es un adulto mayor")
-'''
+
 """
 5) Escribir un programa que permita introducir contraseñas de entre 8 y 14 caracteres
 (incluyendo 8 y 14). Si el usuario ingresa una contraseña de longitud adecuada, imprimir por en
@@ -60,17 +59,17 @@ pantalla "Por favor, ingrese una contraseña de entre 8 y 14 caracteres". Nota: 
 de la función len() en Python para evaluar la cantidad de elementos que tiene un iterable tal
 como una lista o un string.
 """
-'''
+
 contra = input("ingrese una contrasena de entre 8 y 14 caracteres: ")
 
 if len(contra) >= 8 and len(contra) <= 14:
     print("ingreso una contrasena correcta")
 else:
     print("por favor ingrese una contrasena de entre 8 y 14 caracteres")
-'''
+
 #6) Media, mediana y moda de una lista de números aleatorios
 
-'''
+
 import random
 numeros_aleatorios = [random.randint(1, 100) for i in range(50)]
 
@@ -87,7 +86,7 @@ print()
 print(f"la media es: {media} ")
 print(f"la mediana es: {mediana} ")
 print(f"la moda es {moda}: ")
-'''
+
 
 """
 7) Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado
@@ -95,7 +94,7 @@ termina con vocal, añadir un signo de exclamación al final e imprimir el strin
 pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por
 pantalla.
 """
-'''
+
 Frase = input("ingrese una frase o palabra: ")
 vocales = "aeiouAEIOU"
 if Frase[-1] in vocales:
@@ -103,7 +102,7 @@ if Frase[-1] in vocales:
     print(Frase)
 else:
     print(Frase)
-'''
+
 
 """
 8) Escribir un programa que solicite al usuario que ingrese su nombre y el número 1, 2 o 3
@@ -128,3 +127,52 @@ elif opcion == 3:
     print(Nombre.title())
 else:
     print("opcion incorrecta")
+
+
+"""
+9) Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la
+magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado
+por pantalla:
+● Menor que 3: "Muy leve" (imperceptible).
+● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
+● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero
+generalmente no causa daños).
+● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras
+débiles).
+● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
+● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
+"""
+
+terremoto = float(input("ingrese la magnitud del terremoto: "))
+
+if terremoto < 3:
+    print("muy leve")
+elif terremoto >= 3 and terremoto < 4:
+    print("leve")
+elif terremoto >= 4 and terremoto <5:
+    print("moderado")
+elif terremoto >= 5 and terremoto < 6:
+    print("fuerte")
+elif terremoto >= 6 and terremoto < 7:
+    print("muy fuerte")
+else:
+    print("extremo")
+
+"""
+10)Escribir un programa que pregunte al usuario en cuál hemisferio se encuentra (N/S), qué mes
+del año es y qué día es. El programa deberá utilizar esa información para imprimir por pantalla
+si el usuario se encuentra en otoño, invierno, primavera o verano.
+"""
+hemisferiio = input("ingrese en que hemisferio se encuentra (N/S): ")
+mes = int(input("ingrese el mes en numero (1-12): "))
+dia = int(input("ingrese el dia del mes (1-31): "))
+
+if hemisferiio.upper() == "N":
+    if (mes == 12 and dia >= 21) or (mes <= 3 and dia <= 20) or (mes < 3):
+        print("invierno")
+    elif (mes == 3 and dia >= 21) or (mes <= 6 and dia <= 20) or (mes < 6):
+        print("primavera")
+    elif (mes == 6 and dia >= 21) or (mes <= 9 and dia <= 20) or (mes < 9):
+        print("verano")
+    else:
+        print("otoño")
