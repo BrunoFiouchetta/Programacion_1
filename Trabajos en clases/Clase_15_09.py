c8c3d8d3011c59ac3c2bed01c9f3b9d053045442
BrunoFiouchetta <brunofiouchetta12@gmail.com>
2025-09-15 20:32:36 -0300

    avance 3

--- Trabajos en clases/Clase_15_09.py
+++ Trabajos en clases/Clase_15_09.py
@@ -23,23 +23,23 @@ opcion = 0
 
 while opcion != 10:
     print("\n=== BIBLIOTECA ESCOLAR ===")
-    print("1. Ingresar títulos")
+    print("1. Ingresar titulos")
     print("2. Ingresar ejemplares")
     print("3. Mostrar catálogo")
     print("4. Consultar título")
     print("5. Ver agotados")
-    print("6. Agregar título")
+    print("6. Agregar titulo")
     print("7. Ver agotados")
-    print("8. Préstamo/Devolución")
+    print("8. Préstamo/Devolucion")
     print("9. Ver todos los títulos")
     print("10. Salir")
     
     
-    opcion = int(input("Opción: "))
+    opcion = int(input("Opcion: "))
     
     
     if opcion == 1:
-        cantidad = int(input("cuantos títulos va a ingresar? "))
+        cantidad = int(input("cuantos titulos va a ingresar? "))
         for i in range(cantidad):
             titulo = input(f"ingrese el título {i + 1}: ")
             titulos += [titulo]
@@ -47,8 +47,17 @@ while opcion != 10:
 
     elif opcion == 2:
         if not titulos:
-            print("no hay títulos en el catálogo, ingrese títulos primero.")
+            print("no hay titulos en el catalogo, ingrese titulos primero.")
         else:
             for i in range(len(titulos)):
                 ejemplares_libro = int(input(f"ingrese la cantidad de ejemplares para '{titulos[i]}': "))
                 ejemplares[i] += ejemplares_libro
+
+    elif opcion == 3:
+        if not titulos:
+            print("no hay titulos en el catalogo.")
+        else:
+            print("\nCatalogo de libros:")
+            for i in range(len(titulos)):
+                print(f"{titulos[i]}: {ejemplares[i]} ejemplares")
+
