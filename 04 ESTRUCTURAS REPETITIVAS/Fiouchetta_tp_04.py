4ed1873095c27473f8868b468aea0ae050f4ba55
BrunoFiouchetta <brunofiouchetta12@gmail.com>
2025-09-18 21:54:12 -0300

    trabajo 4 terminado

--- 04 ESTRUCTURAS REPETITIVAS/Fiouchetta_tp_04.py
+++ 04 ESTRUCTURAS REPETITIVAS/Fiouchetta_tp_04.py
@@ -74,9 +74,90 @@ print("ACERTASTE!")
 print(f"tardaste {intentos+1} intentos en acertar: ")
 
 
+"""
+6) Desarrolla un programa que imprima en pantalla todos los números pares comprendidos
+entre 0 y 100, en orden decreciente.
+"""
+for i in range(100, -1, -1):
+    if i % 2 == 0:
+        print(i)
 
 
+"""
+7) Crea un programa que calcule la suma de todos los números comprendidos entre 0 y un
+número entero positivo indicado por el usuario.
+"""
+i = 0
+suma = 0
+num_usuario = int(input("ingrese un numero entero positivo: "))
 
+while i != num_usuario:
+    suma += i
+    i+=1
+    print(suma)
+
+
+"""
+8) Escribe un programa que permita al usuario ingresar 100 números enteros. 
+
+Luego, el programa debe indicar cuántos de estos números son pares, cuántos son impares, cuántos son
+negativos y cuántos son positivos. (Nota: para probar el programa puedes usar una cantidad
+menor, pero debe estar preparado para procesar 100 números con un solo cambio).
+"""
+
+num_par = 0
+num_impar = 0
+
+num_posit = 0
+num_neg = 0
+
+for ingreso in range(1, 101):
+    
+    num_usuario = int(input("ingrese un numero entero: "))
+    if num_usuario % 2 == 0 and num_usuario > 0:
+        num_par += 1 
+        num_posit += 1
+    elif num_usuario % 2 != 0 and num_usuario > 0:
+        num_impar += 1
+        num_posit += 1
+    else:
+        num_neg += 1
+
+print(f"ha ingresado {num_posit} numeros positivos.")
+print(f"ha ingresado {num_neg} numeros negativos.")
+print(f"ha ingresado {num_par} numeros pares.")
+print(f"ha ingresado {num_impar} numeros impares.")
+
+
+"""
+9) Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la
+media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe
+poder procesar 100 números cambiando solo un valor).
+"""
+
+import statistics
+lista_nums = []
+
+for ingreso in range(1,101):
+    num_usuario = int(input("ingrese 1 numero entero: "))
+    lista_nums.append(num_usuario)
+
+media = statistics.mean(lista_nums)
+print(f"la media de todos los numeros es {media}")
+
+
+"""
+10) Escribe un programa que invierta el orden de los dígitos de un número ingresado por el
+usuario. Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745.
+"""
+
+num_usuario = (input("ingrese un numero entero: "))
+
+if num_usuario.isdigit():
+    num_inverso = num_usuario[::-1]
+    print(f"Su numero invertido es: {num_inverso}")
+else:
+    print("No ingreso un valor numerico. ")
 
 
 
--- borrador.py
+++ borrador.py
@@ -1,15 +1,8 @@
-import random
+num_usuario = (input("ingrese un numero entero: "))
 
-numero_aleatorio = random.randint(0, 9)
-intentos = 0
-num_usuario = 10
+if num_usuario.isdigit():
+    num_inverso = num_usuario[::-1]
+    print(f"Su numero invertido es: {num_inverso}")
+else:
+    print("No ingreso un valor numerico. ")
 
-while num_usuario != numero_aleatorio:
-
-    num_usuario = int(input("adivina el numero del 1 al 9: "))
-
-    if num_usuario != numero_aleatorio:
-        intentos = intentos +  1
-
-print("ACERTASTE!")
-print(f"tardaste {intentos+1} intentos en acertar: ")
