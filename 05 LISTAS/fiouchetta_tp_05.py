3a870c5fe5f67e52fbdc1225ed413de8e2e882b4
BrunoFiouchetta <brunofiouchetta12@gmail.com>
2025-09-20 15:44:10 -0300

    p6

--- 05 LISTAS/fiouchetta_tp_05.py
+++ 05 LISTAS/fiouchetta_tp_05.py
@@ -36,3 +36,40 @@ lista.append("pato")
 print(lista)
 
 
+"""
+4) Reemplazar el segundo y último valor de la lista “animales” con las palabras “loro” y “oso”,
+respectivamente. Imprimir la lista resultante por pantalla. ¡Puedes hacerlo como se muestra
+en los videos o bien investigar cómo funciona el indexing con números negativos!
+animales = ["perro", "gato", "conejo", "pez"]
+"""
+animales = ["perro", "gato", "conejo", "pez"]
+
+animales[2] = "loro"
+animales[3] = "oso"
+
+print(animales)
+
+
+"""
+5) Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza.
+"""
+#el codigo presentado lo que hace es eliminar el valor maximo dentro de una lista, en el caso del ejemplo elimina el numero 22.
+
+
+"""
+6) Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por
+pantalla los dos primeros.
+"""
+lista = list(range(10,31))
+primeros2 = []
+
+for i in lista:
+    if i % 5 == 0:
+        print(i)
+        if i <= 15:
+            primeros2.append(i)
+
+print(primeros2[0:2])
+
+
+
--- borrador.py
+++ borrador.py
@@ -1,8 +1,12 @@
 
 
-lista=[]
-lista.append("perro")
-lista.append("gato")
-lista.append("pato")
+lista = list(range(10,31))
+primeros2 = []
 
-print(lista)
+for i in lista:
+    if i % 5 == 0:
+        print(i)
+        if i <= 15:
+            primeros2.append(i)
+
+print(primeros2[0:2])
