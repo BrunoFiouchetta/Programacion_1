a4ccb81deac8c1de8d20b1f254bb91989cb630b1
BrunoFiouchetta <brunofiouchetta12@gmail.com>
2025-11-02 20:13:57 -0300

    TP6

--- 06 FUNCIONE/fiouchetta_tp_06.py
+++ 06 FUNCIONE/fiouchetta_tp_06.py
@@ -1,7 +1,7 @@
-print("=== EJERCICIO 1 ===")
+print("==== EJERCICIO 1 ====")
 
 def imprimir_hola_mundo():
-    print("Hola Mundo!")
+    print("Hola Mundo!!")
 
 
 imprimir_hola_mundo()
@@ -126,11 +126,11 @@ print("=== EJERCICIO 10 ===")
 def calcular_promedio(a, b, c):
     return (a + b + c) / 3
 
-a = float(input("Ingrese el primer número: "))
-b = float(input("Ingrese el segundo número: "))
-c = float(input("Ingrese el tercer número: "))
+a = float(input(" Ingrese el primer número: "))
+b = float(input(" Ingrese el segundo número: "))
+c = float(input(" Ingrese el tercer número: "))
 
 promedio = calcular_promedio(a, b, c)
-print(f"El promedio de los tres números es: {promedio}")
+print(f"el promedio de los tres números es: {promedio}")
 print()
 
